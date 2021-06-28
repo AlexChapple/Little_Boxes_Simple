2c93ca9a54f84f28f7e8e2cae5b81e59a1a32f6a
Alex Chapple <alex.a.chapple@gmail.com>
2021-06-28 13:15:51 +1200

    quick update

--- test.jl
+++ test.jl
@@ -2,18 +2,32 @@ using Base.Threads
 
 print(Threads.nthreads())
 
-acc = Ref(0)
+# acc = Ref(0)
 
-@time @threads for i in 1:1000
-    acc[] += 1
+# @time @threads for i in 1:1000
+#     acc[] += 1
+# end
+
+# print(acc)
+
+# acc = Atomic{Int64}(0)
+
+# @time @threads for i in 1:1000
+#     atomic_add!(acc, 1)
+# end
+
+# print(acc)
+
+###
+
+function test()
+    a = zeros(1,5)
+
+    @threads for i in 1:10000
+        a += [1 1 1 1 1]
+    end
+    
+    print(a)
 end
 
-print(acc)
-
-acc = Atomic{Int64}(0)
-
-@time @threads for i in 1:1000
-    atomic_add!(acc, 1)
-end
-
-print(acc)
+test()
